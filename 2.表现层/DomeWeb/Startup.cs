ab22bee6ec9577aaf00ff52bfb3c2bb7fe594ae2
Administrator <Administrator@WQ-20161214HCXN>
2019-04-11 20:46:06 +0800

    添加项目文件。

---
+++ 2.表现层/DomeWeb/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DomeWeb.Startup))]
+namespace DomeWeb
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
